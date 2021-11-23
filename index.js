3bd031d0fb4a5f46dcfc312b9902a37baf8430f7
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-23 10:11:23 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 396;
+const num4 = 14;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 5, 10, 15, 20);
+console.log(max)
+
+
