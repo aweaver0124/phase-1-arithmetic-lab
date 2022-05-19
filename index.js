ece49106dc2ec22c4f24c91243a113ca8a30584e
Alexandra Weaver <aweaver0124@gmail.com>
2022-05-18 19:05:53 -0500

    commit tree 41d8310e89e34b7e5cd801f36425688e49c52677

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const num2 = 2;
 const num3 = 396;
 const num4 = 14;
 
-let multiply = num1 * num2;
+// let multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 10) + 1;
-console.log(random);
+// let random = Math.floor(Math.random() * 10) + 1;
+// console.log(random);
 
-let mod = num3 % num4;
-console.log(mod);
+// let mod = num3 % num4;
+// console.log(mod);
 
-let max = Math.max(1, 5, 10, 15, 20);
-console.log(max)
+// let max = Math.max(1, 5, 10, 15, 20);
+// console.log(max)
 
 
+let multiply = num1 * num2
